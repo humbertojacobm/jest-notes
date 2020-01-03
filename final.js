f92f14f92d3b35aab31840ae519efb7b6f9013d9
humbertojacobm <humbertojacobm@gmail.com>
2020-01-03 18:50:35 -0500

    questions

---
+++ final.js
@@ -0,0 +1,12 @@
+import { notStrictEqual } from "assert"
+
+¿cual es el comando para instalar jest?
+  install jest --save-dev
+
+¿Para ejecutar una sola prueba cual es el comando?
+   era : jest src/__test__/randomStrings.test.js //jest
+¿Shallow nos permite testear un componente?
+ - no se.
+
+¿Que comando utilizamos si queremos regenerar un snapshot?
+ - no es jest updateSnaphost
