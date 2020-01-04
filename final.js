7ed606b9d76b29a394bd70520389049972c51887
humbertojacobm <humbertojacobm@gmail.com>
2020-01-04 17:15:53 -0500

    incremeting the examn

--- final.js
+++ final.js
@@ -1,15 +1,21 @@
 import { notStrictEqual } from "assert"
+import { domainToASCII } from "url"
 
 ¿cual es el comando para instalar jest?
-  install jest --save-dev
+no es: npm install -g jest --save-dev
+no es: npm jest --save-dev
+
+queda: install jest --save-dev
 
 ¿Para ejecutar una sola prueba cual es el comando?
    era : jest src/__test__/randomStrings.test.js //jest
 ¿Shallow nos permite testear un componente?
- - no se.
-     - real unit test, isolation, not children render .
+ - real unit test, isolation, not children render .
  - mount:
     - to test componentdidmount, componentdidupdate. Full rendering including child componentes. Requires DOM .. 
+ - no es : si render del dom 
+ - no es: descomp...
+ -
   
 ¿Que comando utilizamos si queremos regenerar un snapshot?
- - no es jest --updateSnapshot
+ - es jest --updateSnapshot
